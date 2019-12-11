04721a8c9ab8bca2488aeb198f882b8f66b98a69
Anjlika Sahu <anjlikasahu24@gmail.com>
2019-12-11 16:27:16 +0530

    initial commit

---
+++ bot.js
@@ -0,0 +1,13 @@
+var token = '1003018287:AAGG2oM_0Z6uxVA1TlsQ3hQHX5uihq6MVKU';
+
+var Bot = require('node-telegram-bot-api'),
+    bot = new Bot(token, { polling: true });
+
+console.log('bot server started...');
+
+bot.onText(/^\/say_hello (.+)$/, function (msg, match) {
+  var name = match[1];
+  bot.sendMessage(msg.chat.id, 'Hello ' + name + '!').then(function () {
+    // reply sent!
+  });
+});
